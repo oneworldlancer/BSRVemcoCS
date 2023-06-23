8ddf1c55f8a399ed1941c7d5229db4bdbdd4ec16
oneworldlancer <oneworldlancer@gmail.com>
2023-06-23 06:07:55 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BSRVemcoCS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty ( RequestId );
+    }
+}
